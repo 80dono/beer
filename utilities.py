623c19e4150bcdbba404fd6c1a01cd1ee113b58a
80dono <209713000+80dono@users.noreply.github.com>
2026-01-02 19:39:37 +0000

    Set up gspread test

--- utilities.py
+++ utilities.py
@@ -1,8 +1,21 @@
+from dotenv import load_dotenv
+import os
 import pandas as pd
 import gspread
+
+# Load environment variables from .env file
+load_dotenv()
 
 
 def load_beer_data():
     # Generate docstring
     
+    print(f"Attempting to access from {os.getenv('KEY_PATH')}")
+    gc = gspread.service_account(filename=os.getenv("KEY_PATH"))
     
+    gs = gc.open("Beer Database")
+    print(gs)
+    print(type(gs))
+    
+if __name__ == "__main__":
+    load_beer_data()
