16ee34753842dc57cbd00fdc5a1f14770f7c7522
80dono <209713000+80dono@users.noreply.github.com>
2026-07-05 18:03:03 -0400

    Added calorie plots, ruff format

---
+++ README.md
@@ -0,0 +1,2 @@
+If having trouble starting, try reading instructions in beer_key_variable.txt
+- Make sure Docker is running
--- utilities.py
+++ utilities.py
@@ -14,27 +14,29 @@ def load_beer_data() -> pd.DataFrame:
     Returns:
         A dataframe of beer data.
     """
-    
-    key_path = os.getenv('SERVICE_ACCT_CREDS')
+
+    key_path = os.getenv("SERVICE_ACCT_CREDS")
     if key_path is None:
-        raise EnvironmentError("SERVICE_ACCT_CREDS is not set in the local environment.")
+        raise EnvironmentError(
+            "SERVICE_ACCT_CREDS is not set in the local environment."
+        )
     gc = gspread.service_account(filename=key_path)
-    
+
     ws = gc.open("Beer Database").get_worksheet(0)
     data = pd.DataFrame(ws.get_all_records())
-    
+
     # Data cleaning
     df = data.copy()
     df = df.replace("", pd.NA)
-    df["ABV"] = df["ABV"].apply(lambda x: float(x.strip('%'))/100)
-    
+    df["ABV"] = df["ABV"].apply(lambda x: float(x.strip("%")) / 100)
+
     # Break Style into primary and sub-components
     # Use parentheses to denote the part to be returned by str.extract()
     df["main_style"] = df["Style"].str.extract(r"^([^\(]+)", expand=False).str.strip()
     # Might want to make sub_style be "{sub_style} {style}" instead of just "{sub_style}"
     df["sub_style"] = df["Style"].str.extract(r"\(([\D\s]+)\)$", expand=False)
-    print(df)
-     
+    # print(df)
+
     return df
 
 
